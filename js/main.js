ff616aa70fe86377cde866f31de715fbf79deb66
Christian BeauxYeux <gildweek@gmail.com>
2021-11-01 12:18:23 +0400

    initial commit & HTML & CSS oprganisation

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// ***navbar JS***
+const navbar = document.querySelector(".navbar");
+const navWrapper = document.querySelector(".nav-wrapper")
+window.addEventListener("scroll", () => {
+if (window.scrollY >= window.innerHeight / 4) {
+    navbar.classList.add("navbar-white");
+    navWrapper.classList.add("top");
+} else {
+    navbar.classList.remove("navbar-white");
+    navWrapper.classList.remove("top");
+}
+});
+
+
+// ***email JS***
+
+const 
